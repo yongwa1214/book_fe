f74966be6eb9392aee202f873768e12528c7b3ee
yongwa1214 <yongwa1214@gmail.com>
2026-07-23 19:00:02 +0900

    make Service

---
+++ src/services/libraryService.js
@@ -0,0 +1,29 @@
+import axios from './httpRequester';
+
+export const saveBook = args => {
+    return axios.post('save-book', args).catch(e => e.response);
+}
+
+// google books api
+export const bookList = params => {
+    return axios.get('search/book', {params}).catch(e => e.response);
+}
+
+// user library list
+export const myBookList= () =>{
+    return axios.get('my/library').catch(e => e.response);
+}
+
+// one book detail
+export const myBookItem= libraryId =>{
+    return axios.get(`my/library/${libraryId}`).catch(e => e.response);
+}
+
+// my book update
+export const bookUpdate = args => {
+    return axios.put('/my/book', args).catch(e => e.response);
+}
+
+export const removeBook = libraryId => {
+    return axios.delete(`/remove/${libraryId}`).catch(e => e.response);
+}
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,17 +1,104 @@
 <script setup>
+import { reactive } from 'vue';
+
+const state = reactive({
+    keyword: '',
+    page: 1,
+    type: 'title'
+});
+
+const search = () => {
+  console.log(state.keyword);
+  // TODO: 검색 API 호출
+};
+
 </script>
 <template>
     <div class="home">
-        WECOME TO BOBO!!
+        <span>WELCOME TO BOBO!!</span>
     </div>
+    <!-- search area -->
+    <div class="search-area">
+    <div class="search-box">
+      <input
+        type="text"
+        v-model="state.keyword"
+        placeholder="本のタイトルを入力してください。"
+        @keyup.enter="search"
+      />
+
+      <button @click="search" @enter="search">
+             <i class="bi bi-search"></i>
+      </button>
+    </div>
+  </div>
 
 </template>
 <style lang="scss" scoped>
 .home{
+    display: flex;
+    justify-content: center;
+    align-items: center;
     box-sizing: border-box;
     height: 40vh;
     background-color: #c7e2fc;
     color: #0d6efd;
     font-size: 100px;
+    overflow: hidden;   
+    position: relative;
+    
 }
+
+.search-area {
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+}
+
+/* 검색 박스 */
+.search-box {
+  width: 700px;
+  height: 55px;
+
+  display: flex;
+
+  border: 2px solid #adceff;
+  border-radius: 50px;
+
+  overflow: hidden;
+
+  background: white;
+}
+
+/* Input */
+.search-box input {
+  flex: 1;
+
+  border: none;
+  outline: none;
+
+  padding: 0 20px;
+
+  font-size: 18px;
+}
+
+/* 버튼 */
+.search-box button {
+  width: 70px;
+
+  border: none;
+
+ background: #ffffff;
+  color: #5d9cfa;
+
+  font-size: 24px;
+
+  cursor: pointer;
+
+  transition: 0.2s;
+}
+
+// .search-box button:hover {
+//   background: #0056d6;
+// }
 </style>
